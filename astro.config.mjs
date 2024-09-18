3bd3690c3096f0c47f45cbe01487a43f1fc059eb
raruuu <raru@mulisha.com.br>
2024-09-18 18:32:07 -0300

    "Comment out base config option in astro.config.mjs"

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ export default defineConfig({
   integrations: [react(), tailwind()],
     site: 'https://raru.github.io/batucadas0924/',
     // root: './Users/raru/Sites/localhost/batucadas',
-    base: './Users/raru/Sites/localhost/batucadas/dist/',
+    // base: './Users/raru/Sites/localhost/batucadas/dist/',
     output: 'static'  
 });
