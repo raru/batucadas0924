3d64347326c4d21992e7a3aa18b10280635d94dc
raruuu <raru@mulisha.com.br>
2024-09-18 18:01:10 -0300

    changes base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
     site: 'https://raru.github.io/batucadas0924/',
-    root: './batucadas0924'
-    // base: '/batucadas0924',  
+    // root: './Users/raru/Sites/localhost/batucadas',
+    base: './Users/raru/Sites/localhost/batucadas/dist/',
+    output: 'static'  
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="../Users/raru/Sites/localhost/batucadas/.astro/types.d.ts" />
 /// <reference path="../batucadas0924/.astro/types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
