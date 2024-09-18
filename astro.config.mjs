3f7cd7232819d551797f21e0ff1c2d66925ff02e
raruuu <raru@mulisha.com.br>
2024-09-18 17:25:10 -0300

    path sem base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-    site: 'https://raru.github.io',
-    base: '/batucadas0924',  
+    site: 'https://raru.github.io'
+    // base: '/batucadas0924',  
 });
